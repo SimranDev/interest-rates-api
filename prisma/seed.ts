a7e9291b8906cbec0b94df43b494a291502283af
Simran Singh <simran.develop@gmail.com>
2023-09-14 20:34:32 +1200

    seed data updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,49 +8,58 @@ async function main() {
   const anz = await prisma.lender.create({
     data: {
       name: 'ANZ',
+      identifier: 'anz',
     },
   });
   const asb = await prisma.lender.create({
     data: {
       name: 'ASB',
+      identifier: 'asb',
     },
   });
   const westpac = await prisma.lender.create({
     data: {
       name: 'Westpac',
+      identifier: 'westpac',
     },
   });
 
-  await prisma.lenderInterestRate.createMany({
+  await prisma.lenderProducts.createMany({
     data: [
       {
         lenderId: anz.id,
-        rate: '3.5',
+        name: 'Product 1',
+        rate: 3.5,
         createdAt: new Date('2023-01-01T00:00:00.000Z'),
       },
       {
         lenderId: anz.id,
-        rate: '3.6',
+        name: 'Product 2',
+        rate: 3.6,
         createdAt: new Date('2023-01-02T00:00:00.000Z'),
       },
       {
         lenderId: asb.id,
-        rate: '2.8',
+        name: 'Product 1',
+        rate: 2.8,
         createdAt: new Date('2023-01-01T00:00:00.000Z'),
       },
       {
         lenderId: asb.id,
-        rate: '2.9',
+        name: 'Product 2',
+        rate: 2.9,
         createdAt: new Date('2023-01-02T00:00:00.000Z'),
       },
       {
         lenderId: westpac.id,
-        rate: '1.9',
+        name: 'Product 1',
+        rate: 1.9,
         createdAt: new Date('2023-01-01T00:00:00.000Z'),
       },
       {
         lenderId: westpac.id,
-        rate: '2.0',
+        name: 'Product 2',
+        rate: 2.0,
         createdAt: new Date('2023-01-02T00:00:00.000Z'),
       },
     ],
@@ -58,7 +67,7 @@ async function main() {
 }
 
 async function clearDatabase() {
-  await prisma.lenderInterestRate.deleteMany();
+  await prisma.lenderProducts.deleteMany();
   await prisma.lender.deleteMany();
 }
 
