801ed9b8d5750ef9359594333fecfaff76195fb8
Simran Singh <simran.develop@gmail.com>
2023-09-15 00:49:12 +1200

    scraper updated based on schema changes

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -1,50 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import puppeteer from 'puppeteer';
+import * as puppeteer from 'puppeteer';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class ScraperService {
-  @Cron(CronExpression.EVERY_6_HOURS)
-  async getLenderRates() {
+  private readonly logger = new Logger(ScraperService.name);
+
+  constructor(private prisma: PrismaService) {}
+
+  @Cron(CronExpression.EVERY_12_HOURS)
+  async handleCron() {
+    this.logger.debug('Cron job started');
+    try {
+      await this.scrapeAndUpsertData();
+    } catch (error) {
+      this.logger.error('Failed to scrape and upsert data', error.stack);
+    }
+  }
+
+  async scrapeAndUpsertData() {
     let browser;
+
     try {
       browser = await puppeteer.launch({ headless: 'new' });
       const page = await browser.newPage();
-
       await page.goto(process.env.SCRAPE_URL);
 
       const data = await page.evaluate(() => {
-        const rows = Array.from(
-          document.querySelectorAll('#tablepress-all tbody tr'),
-        );
-        console.log('rows', rows);
+        function extractBankName(url: string) {
+          const parts = url.split('/');
+          const logoFilename = parts[parts.length - 1];
+          return logoFilename.split('.')[0];
+        }
 
-        const headers = Array.from(
-          document.querySelectorAll('#tablepress-all thead th'),
-        ).map((header) => header.textContent.trim());
-
-        console.log('headers', headers);
-
-        return rows.map((row) => {
-          const cells = Array.from(row.querySelectorAll('td'));
-          const rowData = {};
-          cells.forEach((cell, index) => {
-            const img = cell.querySelector('img');
-            if (img && index === 0) {
-              rowData[headers[index]] = img.src.split('/').pop().split('.')[0]; // Get bank name from the img src
-            } else {
-              rowData[headers[index]] = cell.textContent.trim();
+        const rows = document.querySelectorAll('#tablepress-all tbody tr');
+        return Array.from(rows, (row) => {
+          const columns = row.querySelectorAll('td');
+          return Array.from(columns, (column, index) => {
+            if (index === 0) {
+              const img = column.querySelector('img');
+              return img ? extractBankName(img.src) : null;
             }
+            return column.innerText;
           });
-          return rowData;
         });
       });
 
-      console.log('data', data);
-      return data;
+      this.logger.log('Scraped data', data);
+
+      for (const row of data) {
+        const lenderName = row[0];
+        const identifier = lenderName
+          .replace(/[^a-zA-Z0-9]/g, '')
+          .toLowerCase();
+
+        const lender = await this.prisma.lender.upsert({
+          where: { identifier },
+          update: { name: lenderName, updatedAt: new Date() },
+          create: { identifier, name: lenderName },
+        });
+
+        await this.prisma.loanProduct.upsert({
+          where: {
+            name_lenderId: {
+              name: 'Standard',
+              lenderId: lender.id,
+            },
+          },
+          update: {
+            floating: parseFloat(row[1]) || null,
+            fixed12Months: parseFloat(row[2]) || null,
+            fixed24Months: parseFloat(row[3]) || null,
+            fixed36Months: parseFloat(row[4]) || null,
+            fixed48Months: parseFloat(row[5]) || null,
+            fixed60Months: parseFloat(row[6]) || null,
+            updatedAt: new Date(),
+          },
+          create: {
+            name: 'Standard',
+            lenderId: lender.id,
+            floating: parseFloat(row[1]) || null,
+            fixed12Months: parseFloat(row[2]) || null,
+            fixed24Months: parseFloat(row[3]) || null,
+            fixed36Months: parseFloat(row[4]) || null,
+            fixed48Months: parseFloat(row[5]) || null,
+            fixed60Months: parseFloat(row[6]) || null,
+          },
+        });
+      }
     } catch (error) {
-      console.error('An error occurred:', error);
-      return [];
+      this.logger.error('Scraping or upserting failed', error.stack);
     } finally {
       if (browser) {
         await browser.close();
